df3c4e8331f4bbc5b40e2c39f98bc63979dcb1db
junwoochoi <cjw4690@gmail.com>
2020-01-24 16:44:33 +0900

    feat : add kapt plugin to annotationProcessing

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,24 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.2.4.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
-    id ("org.jetbrains.kotlin.plugin.jpa") version "1.3.61"
+    kotlin("plugin.jpa") version "1.3.61"
     kotlin("plugin.spring") version "1.3.61"
     kotlin("jvm") version "1.3.61"
+    kotlin("kapt") version "1.3.61"
+}
+
+
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.Embeddable")
+    annotation("javax.persistence.MappedSuperclass")
 }
 
 group = "com.junu"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
+
 
 repositories {
     mavenCentral()
@@ -23,6 +33,7 @@ dependencies {
     implementation("com.h2database:h2")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
