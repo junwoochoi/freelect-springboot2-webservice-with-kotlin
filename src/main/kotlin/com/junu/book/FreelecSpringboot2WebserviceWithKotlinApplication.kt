e2752486c08561bffd78cf3cc852b319ef7cd35b
junwoochoi <cjw4690@gmail.com>
2020-01-24 14:45:44 +0900

    Initial commit

---
+++ src/main/kotlin/com/junu/book/FreelecSpringboot2WebserviceWithKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.junu.book
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FreelecSpringboot2WebserviceWithKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<FreelecSpringboot2WebserviceWithKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/junu/book/FreelecSpringboot2WebserviceWithKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.junu.book
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FreelecSpringboot2WebserviceWithKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
