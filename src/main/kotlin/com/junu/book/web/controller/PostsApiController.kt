9bc0af95215aaa875645e36955d5c9b37b89b17e
junwoochoi <cjw4690@gmail.com>
2020-01-24 23:12:10 +0900

    bugfix : change PUT /api/v1/posts -> POST

--- src/main/kotlin/com/junu/book/web/controller/PostsApiController.kt
+++ src/main/kotlin/com/junu/book/web/controller/PostsApiController.kt
@@ -2,13 +2,13 @@ package com.junu.book.web.controller
 
 import com.junu.book.service.posts.PostsService
 import com.junu.book.web.dto.PostsSaveRequestDto
-import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class PostsApiController(private val postsService: PostsService) {
 
-    @PutMapping("/api/v1/posts")
+    @PostMapping("/api/v1/posts")
     fun save(@RequestBody postsSaveRequestDto: PostsSaveRequestDto): Long? = postsService.save(postsSaveRequestDto)
 }
