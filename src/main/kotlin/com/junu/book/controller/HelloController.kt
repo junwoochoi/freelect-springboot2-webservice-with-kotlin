27ed963d607d874ec7ce6a0f0b2c6444a732f468
junwoochoi <cjw4690@gmail.com>
2020-01-24 15:16:24 +0900

    feat : change parameters of GET /hello and accept DSL in WebMvcTest

--- src/main/kotlin/com/junu/book/controller/HelloController.kt
+++ src/main/kotlin/com/junu/book/controller/HelloController.kt
@@ -3,13 +3,15 @@ package com.junu.book.controller
 import com.junu.book.dto.HelloResponseDto
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloController {
 
     @GetMapping("/hello")
-    fun hello() = ResponseEntity.ok(HelloResponseDto("hello", 5))
+    fun hello(@RequestParam name : String, @RequestParam amount : Int) =
+            ResponseEntity.ok(HelloResponseDto(name, amount))
 
 
 }
--- src/test/kotlin/com/junu/book/controller/HelloControllerTest.kt
+++ src/test/kotlin/com/junu/book/controller/HelloControllerTest.kt
@@ -1,30 +1,44 @@
 package com.junu.book.controller
 
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.TestConstructor
-import org.springframework.test.web.client.match.MockRestRequestMatchers.content
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.get
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 internal class HelloControllerTest(private val mockMvc: MockMvc) {
 
-    @Test
-    @DisplayName("GET /hello 는 'hello'를 리턴한다.")
-    fun testHello() {
-        mockMvc.perform(get("/hello"))
-                .andDo(print())
+    @ParameterizedTest
+    @MethodSource("providerParams")
+    @DisplayName("GET /hello 는 받아온 name, amount를 리턴한다.")
+    fun testHello(name: String, amount: Int) {
+        mockMvc.get("/hello"){
+            param("name", name)
+            param("amount", amount.toString())
+        }
+                .andDo { print() }
                 .andExpect {
-                    status().isOk
-                    content().string("hello")
+                    status { isOk }
+                    jsonPath("$.name"){value(name)}
+                    jsonPath("$.amount"){value(amount)}
                 }
+    }
+
+    companion object {
+        @JvmStatic
+        fun providerParams() = listOf(
+                Arguments.of("hi", 2000),
+                Arguments.of("hello", 10000),
+                Arguments.of("who", 20000),
+                Arguments.of("you", 15000)
+        )
     }
 
 }
