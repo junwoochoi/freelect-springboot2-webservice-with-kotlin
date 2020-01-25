01215ce3f31df58ce548c369ac11ad1085aa945d
junwoochoi <cjw4690@gmail.com>
2020-01-25 17:50:19 +0900

    bugfix : fix webmvctest fail issue

--- src/main/kotlin/com/junu/book/FreelecSpringboot2WebserviceWithKotlinApplication.kt
+++ src/main/kotlin/com/junu/book/FreelecSpringboot2WebserviceWithKotlinApplication.kt
@@ -2,9 +2,7 @@ package com.junu.book
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
-@EnableJpaAuditing
 @SpringBootApplication
 class FreelecSpringboot2WebserviceWithKotlinApplication
 
---
+++ src/main/kotlin/com/junu/book/config/AppConfig.kt
@@ -0,0 +1,9 @@
+package com.junu.book.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@Configuration
+class AppConfig {
+}
