5df85af5564987bf41566553bbe8e4234e28c4a7
최준우 <junwoochoi@zuminternet.com>
2020-01-29 14:42:30 +0900

    feat : add Naver Login Support

--- src/main/kotlin/com/junu/book/config/auth/OAuthAttributes.kt
+++ src/main/kotlin/com/junu/book/config/auth/OAuthAttributes.kt
@@ -2,6 +2,7 @@ package com.junu.book.config.auth
 
 import com.junu.book.domain.user.Role
 import com.junu.book.domain.user.User
+import java.lang.RuntimeException
 
 class OAuthAttributes private constructor(
         val attributes: Map<String, Any>,
@@ -14,10 +15,25 @@ class OAuthAttributes private constructor(
 
     companion object {
         fun of(registrationId: String, userNameAttributeName: String, attributes: Map<String, Any>): OAuthAttributes {
+            if (registrationId == "naver") {
+                return ofNaver("id", attributes)
+            }
             return ofGoogle(userNameAttributeName, attributes)
         }
 
-        fun ofGoogle(userNameAttributeName: String, attributes: Map<String, Any>): OAuthAttributes {
+        @Suppress("UNCHECKED_CAST")
+        private fun ofNaver(userNameAttributeName: String, attributes: Map<String, Any>): OAuthAttributes {
+            val response: Map<String, Any> = attributes["response"] as Map<String, Any>
+            return OAuthAttributes(
+                    name = response["name"] as String,
+                    email = response["email"] as String,
+                    picture = response.getOrDefault("profile_image", "") as String,
+                    attributes = response,
+                    nameAttributeKey = userNameAttributeName
+            )
+        }
+
+        private fun ofGoogle(userNameAttributeName: String, attributes: Map<String, Any>): OAuthAttributes {
             return OAuthAttributes(
                     name = attributes["name"] as String,
                     email = attributes["email"] as String,
@@ -26,6 +42,7 @@ class OAuthAttributes private constructor(
                     nameAttributeKey = userNameAttributeName
             )
         }
+
     }
 
 }
